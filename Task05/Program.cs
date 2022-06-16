b34b5ef5ada7710b3808e31b85bbc307b702c5ea
Oleksii Hnativ <boss2003bobo@gmail.com>
2022-06-16 20:00:45 +0300

    merge sort added

--- Task05/Program.cs
+++ Task05/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Diagnostics;
 
 namespace Task05
 {
@@ -6,10 +8,46 @@ namespace Task05
     {
         static void Main(string[] args)
         {
-			Vector vect = new Vector(10);
-            vect.RandomInitialization();
-            vect.HeapSort();
-            Console.WriteLine(vect);
-		}
+            try
+            {
+                Vector vect = new Vector(10);
+                vect.RandomInitialization();
+                vect.HeapSort();
+                Console.WriteLine(vect);
+                Vector vector = new Vector("../../../file.txt", "../../../part1.txt", "../../../part2.txt");
+                vector.MergeSort("../../../part1.txt", "../../../part2.txt", "../../../result.txt");
+
+                Stopwatch timer = new Stopwatch();
+
+                Vector vectMergeCheck = new Vector(100);
+                vectMergeCheck.RandomInitialization();
+                timer.Start();
+                vectMergeCheck.MergeSort();
+                timer.Stop();
+                Console.WriteLine(vectMergeCheck);
+                Console.WriteLine($"Time taken for merge sort: {timer.Elapsed.TotalMilliseconds}");
+
+                timer.Reset();
+                vectMergeCheck.RandomInitialization();
+                timer.Start();
+                vectMergeCheck.MergeSortOptimal();
+                timer.Stop();
+                Console.WriteLine(vectMergeCheck);
+                Console.WriteLine($"Time taken for optimal merge sort: {timer.Elapsed.TotalMilliseconds}");
+
+                Vector check1 = new Vector(new int[] { 1, 2, 3 });
+                Vector check2 = new Vector(new int[] { 1, 3, 3 });
+                Console.WriteLine(check1 == check2);
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine($"File {e.FileName} not found");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
     }
 }
--- Task05/Vector.cs
+++ Task05/Vector.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
 
 namespace Task05
 {
@@ -33,11 +35,35 @@ namespace Task05
         { }
         public Vector(int[] arr)
         {
-            this.arr = arr;
+            this.arr = arr ?? throw new ArgumentNullException("No data in array given");
         }
         public Vector(int n)
         {
-            arr = new int[n];
+            try
+            {
+                arr = new int[n];
+            }
+            catch (OverflowException)
+            {
+                throw new ArgumentException("Value for size of array is incorrect!");
+            }
+        }
+        public Vector(in string fileName)
+        {
+            arr = GetParsed(fileName);
+        }
+        public Vector(in string fileGiven, in string lhs, in string rhs)
+        {
+            StreamWriter writer = new StreamWriter(lhs);
+            WriteToFile(writer, GetParsed(fileGiven), 0, GetParsed(fileGiven).Length / 2);
+            writer.Close();
+            writer = new StreamWriter(rhs);
+            WriteToFile(writer, 
+                GetParsed(fileGiven), 
+                GetParsed(fileGiven).Length / 2, 
+                GetParsed(fileGiven).Length);
+            writer.Close();
+            arr = null;
         }
         public void RandomInitialization(int a, int b)
         {
@@ -73,7 +99,66 @@ namespace Task05
                 }
             }
         }
-        private int GetIndexOfPivot(in int left, in int right, Type sortType)
+        public void QuickSort(Type type = Type.Middle)
+        {
+            GetSortedArray(0, arr.Length - 1, type);
+        }
+        public void HeapSort()
+        {
+            for (int i = arr.Length / 2 - 1; i >= 0; i--)
+                GetSubTree(arr.Length, i);
+
+            for (int i = arr.Length - 1; i > 0; i--)
+            {
+                Swap(ref arr[0], ref arr[i]);
+                GetSubTree(i, 0);
+            }
+        }
+        public void MergeSort(in string lhs, in string rhs, in string sortedFile)
+        {
+            SaveSorted(lhs);
+            SaveSorted(rhs);
+
+            string[] file1 = File.ReadAllLines(lhs);
+            string[] file2 = File.ReadAllLines(rhs);
+            string[] result = file1.Concat(file2).ToArray();
+            int[] res = new int[result.Length];
+            for (int j = 0; j < result.Length; j++)
+            {
+                res[j] = int.Parse(result[j]);
+            }
+            Vector newVect = new Vector(res);
+            newVect.MergeSort();
+            newVect.Save(sortedFile);
+        }
+        public void MergeSort()
+        {
+            MergeSortRange(0, GetLength() - 1);
+        }
+        public void MergeSortOptimal()
+        {
+            MergeSortOptimal(0, GetLength() - 1);
+        }
+        private void GetSubTree(in int n, in int subRoot)
+        {
+            int rootNode = subRoot; 
+            int left = 2 * subRoot + 1; 
+            int right = 2 * subRoot + 2; 
+
+            if (left < n && arr[left] > arr[rootNode])
+                rootNode = left;
+
+            if (right < n && arr[right] > arr[rootNode])
+                rootNode = right;
+
+            if (rootNode != subRoot)
+            {
+                Swap(ref arr[subRoot], ref arr[rootNode]);
+
+                GetSubTree(n, rootNode);
+            }
+        }
+        private int GetIndexOfPivot(in int left, in int right, in Type sortType)
         {
             return sortType switch
             {
@@ -115,38 +200,143 @@ namespace Task05
             GetSortedArray(left, j, sortType);
             GetSortedArray(i, right, sortType);
         }
-        public void QuickSort(Type type = Type.Middle)
+        private int GetLength()
         {
-            GetSortedArray(0, arr.Length - 1, type);
+            return arr.Length;
         }
-        public void HeapSort()
+        private int[] GetParsed(in string fileGiven)
         {
-            for (int i = arr.Length / 2 - 1; i >= 0; i--)
-                GetSubTree(arr.Length, i);
-
-            for (int i = arr.Length - 1; i > 0; i--)
+            string[] lines = File.ReadAllLines(fileGiven);
+            if(lines.Length == 0)
             {
-                Swap(ref arr[0], ref arr[i]);
-                GetSubTree(i, 0);
+                throw new ArgumentNullException("No objects in file given");
+            }
+            int[] arrParsed = new int[lines.Length];
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                if (!int.TryParse(lines[i], out arrParsed[i]))
+                {
+                    throw new ArgumentException("Data in array cannot be parsed");
+                }
+                else if (lines[i] == null)
+                {
+                    throw new ArgumentNullException("Problems in file");
+                }
+            }
+            return arrParsed;
+        }
+        private void WriteToFile(in StreamWriter writer, 
+            in int[] arrTo, in int start, in int end)
+        {
+            for (int i = start; i < end; i++)
+            {
+                writer.WriteLine(arrTo[i]);
             }
         }
-        void GetSubTree(int n, int subRoot)
+        private void SaveSorted(in string fileName)
         {
-            int rootNode = subRoot; 
-            int left = 2 * subRoot + 1; 
-            int right = 2 * subRoot + 2; 
-
-            if (left < n && arr[left] > arr[rootNode])
-                rootNode = left;
-
-            if (right < n && arr[right] > arr[rootNode])
-                rootNode = right;
-
-            if (rootNode != subRoot)
+            Vector tmp = new Vector(fileName);
+            tmp.MergeSort();
+            tmp.Save(fileName);
+        }       
+        private void Save(in string file) 
+        {
+            StreamWriter writer = new StreamWriter(file);
+            WriteToFile(writer, arr, 0, GetLength());
+            writer.Close();
+        }
+        private void Merge(in int lhs, in int middle, in int rhs) 
+        {
+            int i = lhs;
+            int j = middle + 1;
+            int[] temp = new int[rhs - lhs + 1];
+            int k = 0;
+            while (i <= middle && j <= rhs)
             {
-                Swap(ref arr[subRoot], ref arr[rootNode]);
-
-                GetSubTree(n, rootNode);
+                if (arr[i] < arr[j])
+                {
+                    temp[k++] = arr[i++];
+                }
+                else
+                {
+                    temp[k++] = arr[j++];
+                }
+            }
+            if (i > middle)
+            {
+                while (j <= rhs)
+                {
+                    temp[k++] = arr[j++];
+                }
+            }
+            else
+            {
+                while (i <= middle)
+                {
+                    temp[k++] = arr[i++];
+                }
+            }
+            for (int n = 0; n < temp.Length; n++)
+            {
+                arr[n + lhs] = temp[n];
+            }
+        }
+        private void MergeSortRange(in int start, in int end) 
+        {
+            if (end - start <= 1) 
+            {
+                if (end != start && arr[end] < arr[start]) 
+                {
+                    Swap(ref arr[end], ref arr[start]);
+                }
+                return;
+            }
+            int middle = (end + start) / 2;
+            MergeSortRange(start, middle);
+            MergeSortRange(middle + 1, end);
+            Merge(start, middle, end);
+        }
+        private void MergeSortOptimal(in int left, in int right)
+        {
+            if (left < right)
+            {
+                int middle = left + (right - left) / 2;
+                MergeSortOptimal(left, middle);
+                MergeSortOptimal(middle + 1, right);
+                MergeOptimal(left, middle, right);
+            }
+        }
+        private void MergeOptimal(in int left, in int middle, in int right)
+        {
+            int[] leftTempArray = new int[middle - left + 1];
+            int[] rightTempArray = new int[right - middle];
+            int i, j;
+            for (i = 0; i < middle - left + 1; ++i)
+                leftTempArray[i] = arr[left + i];
+            for (j = 0; j < right - middle; ++j)
+                rightTempArray[j] = arr[middle + 1 + j];
+            i = 0;
+            j = 0;
+            int k = left;
+            while (i < middle - left + 1 && j < right - middle)
+            {
+                if (leftTempArray[i] <= rightTempArray[j])
+                {
+                    arr[k++] = leftTempArray[i++];
+                }
+                else
+                {
+                    arr[k++] = rightTempArray[j++];
+                }
+            }
+            while (i < middle - left + 1)
+            {
+                arr[k++] = leftTempArray[i++];
+            }
+            while (j < right - middle)
+            {
+                arr[k++] = rightTempArray[j++];
             }
         }
         public override string ToString()
@@ -157,6 +347,23 @@ namespace Task05
                 str += arr[i] + " ";
             }
             return str;
+        }
+        public override bool Equals(object obj)
+        {
+            return obj is Vector vector &&
+                   arr.SequenceEqual(vector.arr);
+        }
+        public override int GetHashCode()
+        {
+            return arr.GetHashCode();
+        }
+        public static bool operator ==(Vector left, Vector right)
+        {
+            return left.Equals(right);
+        }
+        public static bool operator !=(Vector left, Vector right)
+        {
+            return !(left == right);
         }
     }
 }
