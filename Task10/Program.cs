699ec4f3dedf00a41d06546ab1e277596e2e4bda
Oleksii Hnativ <boss2003bobo@gmail.com>
2022-06-21 22:57:10 +0300

    task10 is not ready yet

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -6,7 +6,7 @@ namespace Task10
     {
         static void Main(string[] args)
         {
-            Matrix matrix = new Matrix(5, 7);
+            Matrix matrix = new Matrix(7, 5);
             foreach (int item in matrix)
             {
                 Console.WriteLine(item);
