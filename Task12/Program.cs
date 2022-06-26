9229d953d73a62aacf5a88857ab7959360084229
Oleksii Hnativ <boss2003bobo@gmail.com>
2022-06-26 13:49:23 +0300

    task is not ready yet

---
+++ Task12/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Task12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
